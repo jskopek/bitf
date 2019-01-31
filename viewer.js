feb04c908b7ea1018d2c5a07a0eaf8fcb6e6c42e
Jean-Marc Skopek <jskopek@gmail.com>
2019-01-31 02:19:18 -0800

    Viewer now displays room and supports VR

--- viewer.js
+++ viewer.js
@@ -1,4 +1,3 @@
-var THREE = require('three');
 var _ = require('lodash');
 var dat = require('dat.gui');
 var Panel = require('./modules/panel.js');
@@ -6,34 +5,53 @@ var Ceiling = require('./modules/ceiling.js');
 var Sequence = require('./modules/sequence.js');
 
 
-}
-
-
-
-
-
 // initialize
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.z = 100;
-camera.position.x = 30;
-camera.position.y = 30;
+scene.background = new THREE.Color( 0x505050 );
+
+// room
+camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.1, 10 );
+camera.rotation.x = 1;
+
+
+room = new THREE.LineSegments(
+    new THREE.BoxLineGeometry( 10, 6, 10, 10, 10, 10 ), //x, z, y
+    new THREE.LineBasicMaterial( { color: 0x808080 } )
+);
+room.geometry.translate( 0, 3, 0 );
+scene.add( room );
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setPixelRatio( window.devicePixelRatio );
 document.body.appendChild( renderer.domElement );
 
-var ceiling = new Ceiling(scene, 10, 10, 8, 2);
+// VR
+renderer.vr.enabled = true;
+document.body.appendChild( WEBVR.createButton( renderer ) );
 
-function animate() {
-    requestAnimationFrame( animate );
-    ceiling.animate();
+
+//var controls = new THREE.TrackballControls(camera, renderer.domElement);
+//controls.rotateSpeed = 1.0;
+//controls.zoomSpeed = 1.2;
+//controls.panSpeed = 0.8;
+//controls.noZoom = false;
+//controls.noPan = false;
+//controls.staticMoving = true;
+//controls.dynamicDampingFactor = 0.3;
+//controls.keys = [ 65, 83, 68 ];
+
+renderer.setAnimationLoop(() => {
     renderer.render( scene, camera );
-}
-animate();
+    //ceiling.animate();
+    //controls.update();
+});
 
-
-
+// Initialize scene
+var offsetX = -5;
+var offsetY = -5;
+var offsetZ = 6;
+var ceiling = new Ceiling(scene, 10, 10, 1, 0.1, offsetX, offsetY, offsetZ);
 var sequence = new Sequence(ceiling);
 
 // GUI
@@ -53,6 +71,12 @@ sequenceRunnerGUI.add(sequence, 'boomerang')
 sequenceRunnerGUI.add(sequence, 'directionForward').listen();
 sequenceRunnerGUI.add(sequence, 'playSpeed', 100, 3000)
 sequenceRunnerGUI.open();
+
+var cameraGUI = gui.addFolder('Camera');
+cameraGUI.add(camera.rotation, 'x', -3.14 * 2, 3.14).listen()
+cameraGUI.add(camera.rotation, 'y', -3.14, 3.14).listen()
+
+cameraGUI.open();
 
 // load dropped sequences
 window.addEventListener("dragover", (e) => { e.preventDefault(); },false);
