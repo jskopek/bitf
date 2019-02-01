1f537a4beb4a2b92a261a7f35c715789b7ab351c
Jean-Marc Skopek <jskopek@gmail.com>
2019-01-31 21:57:17 -0800

    Port may be configured in environment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const Pusher = require('pusher');
 const express = require('express');
 const app = express()
-const port = 3000
+const PORT = process.env.PORT || 3000
 
 app.set('view engine', 'ejs');
 
