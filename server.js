b05bf443eceb955b8a2b0c88edbee5845892fe6f
Jean-Marc Skopek <jskopek@gmail.com>
2019-07-10 00:59:32 -0700

    Renaming `controller.js` to `server.js`

--- server.js
+++ server.js
@@ -2,32 +2,38 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 
-// initialize socket server
-var io = require('socket.io')(server);
-var socket = undefined;
-//io.on('connection', (newSocket) => { 
-//    socket = newSocket; 
-//    socket.on('render', (values) => {
-//        console.log('render received!', values);
-//    });
-//});
 
+// ---- BONJOUR-BASED PANEL COMMUNICATION --------------------------------------
 // initialize bonjour panel manager
 var {BonjourPanelManager} = require('./modules/bonjourPanelManager.js');
 var panelManager = new BonjourPanelManager();
+// ---- END BONJOUR-BASED PANEL COMMUNICATION ----------------------------------
 
+// (or)
 
+// ---- ARDUINO SERIAL-BASED PANEL COMMUNICATION -------------------------------
 // initialize arduino panel manager
 // var ArduinoPanelManager = require('./modules/arduinoPanelManager.js');
 // var panelManager = new ArduinoPanelManager('/dev/ttyS3', 9600);
+// ---- END ARDUINO SERIAL-BASED PANEL COMMUNICATION ---------------------------
 
+
+
+// ---- SOCKET SERVER: LISTENS FOR COMMANDS FROM CONTROLLER GUI ----------------
+// initialize socket server
+var io = require('socket.io')(server);
+var socket = undefined;
 io.on('connection', (newSocket) => { 
+    socket = newSocket
     newSocket.on('render', (ledMatrix) => {
+        console.log('socket.render', ledMatrix)
         panelManager.send(ledMatrix);
     });
 });
 
 
+
+// ---- LAUNCHPAD CONTROLLER ----------------------------------------------------
 //// initialize launchpad controller
 //var LaunchpadController = require('./modules/launchpad.js');
 //var launchpad = new LaunchpadController('./sequences/');
@@ -39,15 +45,15 @@ io.on('connection', (newSocket) => {
 //    console.log('launchpad.configuration', configuration);
 //    if(socket) { socket.emit('configuration', configuration); }
 //});
-//
-//var BonjourPanelFinder = require('./modules/bonjourPanelFinder.js');
-//var bonjourPanels = new BonjourPanelFinder(socket);
+// ---- END LAUNCHPAD CONTROLLER -----------------------------------------------
 
+
+
+// ---- CONTROLLER GUI WEB SERVER ----------------------------------------------
 // initialize web server
 app.set('view engine', 'ejs')
 app.use(express.static(__dirname));
 app.get('/', (req, res) => { res.render('index'); });
 var port = process.argv[2] || 3000;
 server.listen(port, () => console.log(`Controller listening on port ${port}!`));
-
-
+// ---- END CONTROLLER GUI WEB SERVER ------------------------------------------
